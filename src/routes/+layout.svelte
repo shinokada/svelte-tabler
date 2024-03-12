5e28198465b0f9baf83f7989169f3c78cd627474
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-03-12 20:27:34 +0100

    feat: add ko-fi banner, github issue template, update footer, change to codewithshin.com

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -30,7 +30,7 @@
   description="Tabler icon set for Svelte"
   openGraph={{
     type: 'website',
-    url: 'https://svelte-tabler.vercel.app/',
+    url: 'https://svelte-tabler.codewithshin.com/',
     title: 'Svelte Tabler',
     description: 'Tabler icon set for Svelte',
     images: [
@@ -79,7 +79,7 @@
         <NavLi class="lg:px-2 lg:mb-0" href="https://github.com/shinokada/svelte-tabler"
           >GitHub</NavLi
         >
-        <NavLi href="https://svelte-svg-icons.vercel.app/">Icon sets</NavLi>
+        <NavLi href="https://svelte-svg-icons.codewithshin.com/">Icon sets</NavLi>
       </NavUl>
       <div class="flex items-center ml-auto">
         <DarkMode class="inline-block dark:hover:text-white hover:text-gray-900" />
@@ -94,8 +94,8 @@
   <Footer footerType="logo" class="dark:bg-zinc-950">
     <div class="sm:flex sm:items-center sm:justify-between">
       <FooterBrand
-        href="https://svelte-tabler.vercel.app/"
-        name="Svelte Tabler"
+        href="https://codewithshin.com/"
+        name="codewithshin.com"
         classSpan="text-primary-700 dark:text-primary-500"
       />
       <FooterLinkGroup
@@ -106,7 +106,7 @@
         <FooterLink class="lg:px-2 lg:mb-0" href="https://github.com/shinokada/svelte-tabler"
           >GitHub</FooterLink
         >
-        <FooterLink href="https://svelte-svg-icons.vercel.app/">Icon sets</FooterLink>
+        <FooterLink href="https://svelte-svg-icons.codewithshin.com/">Icon sets</FooterLink>
       </FooterLinkGroup>
     </div>
   </Footer>
