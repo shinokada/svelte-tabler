10965d3676ee4ff3b6f6ba455aa8fcacdd4be336
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2025-03-10 20:13:27 +0100

    fix: update svelte and tailwindcss 4

---
+++ src/routes/testdir/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import { Accessible } from '$lib';
+  import MyIcon from './MyIcon.svelte'
+</script>
+
+<div class="p-16">
+  <h1>Test</h1>
+  <MyIcon Icon={Accessible} />
+</div>
